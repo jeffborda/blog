c9b11bd6d26dc52af435048137664f93d618d3d6
Jeff Borda <jeffborda@outlook.com>
2019-10-11 15:38:10 -0700

    set up unfollowing and checking for unique usernames

--- src/main/java/com/jeffborda/messenger/controllers/HomeController.java
+++ src/main/java/com/jeffborda/messenger/controllers/HomeController.java
@@ -1,13 +1,32 @@
 package com.jeffborda.messenger.controllers;
 
+import com.jeffborda.messenger.models.ApplicationUser;
+import com.jeffborda.messenger.models.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    ApplicationUserRepository applicationUserRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/")
     public String getHome(Principal p, Model m) {
@@ -17,11 +36,56 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/login/failure")
-    public String getLoginFailure(Principal p, Model m) {
+    @GetMapping("/signup")
+    public RedirectView getSignup() {
+        return new RedirectView("/");
+    }
+
+    @PostMapping("/signup")
+    public RedirectView createNewUser(String username, String password) throws Exception {
+        ApplicationUser newUser = new ApplicationUser(username, passwordEncoder.encode(password));
+
+        List<ApplicationUser> currentUsers = applicationUserRepo.findAll();
+        Set<String> uniqueUsernames = new HashSet<>();
+        for(ApplicationUser user : currentUsers) {
+            uniqueUsernames.add(user.getUsername());
+        }
+        if(uniqueUsernames.contains(newUser.getUsername())) {
+            return new RedirectView("/login/username-taken");
+        }
+        else {
+            applicationUserRepo.save(newUser);
+        }
+        // To automatically log user in when registering
+        Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return new RedirectView("/my-profile");
+    }
+
+    @GetMapping("/login/username-taken")
+    public String getUsernameTaken(Principal p, Model m) {
         if(p != null) {
+            // For nav fragment
             m.addAttribute("username", p.getName());
         }
-        return "login-failure";
+        return "login-username-taken";
     }
+
+    @GetMapping("/login")
+    public String getLoginPage(Principal p, Model m) {
+        if(p != null) {
+            // For nav fragment
+            m.addAttribute("username", p.getName());
+        }
+        return "login";
+    }
+
+    //    @GetMapping("/login/failure")
+//    public String getLoginFailure(Principal p, Model m) {
+//        if(p != null) {
+//            m.addAttribute("username", p.getName());
+//        }
+//        return "login-failure";
+//    }
 }
